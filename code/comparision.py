b000061e06c3add60e65107c24ad1a2e86e3c25a
Imran Raja <irsb2009@gmail.com>
2017-09-19 22:12:42 +0600

    Add files via upload

---
+++ code/comparision.py
@@ -0,0 +1,40 @@
+'''
+@author: Imran Raja Singh Bhuiyan
+@maintainer: Imran Raja Singh Bhuiyan
+@date: 19.07.2017
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def autolabel(rects):
+    for rect in rects:
+        height = rect.get_height()
+        ax.text(rect.get_x() + rect.get_width()/2., 1.02*height,
+                '%.2f' % (height),
+                ha='center', va='bottom')
+
+N = 5
+acc_wo_stan = (60.28, 64.57, 80.09, 73.17, 66.10) #without standardization
+acc_w_stan = (60.38, 55.11, 80.09, 71.76, 43.36) #with standardization
+
+ind = np.arange(N)  # the x locations for the groups
+width = 0.35       # the width of the bars
+
+fig, ax = plt.subplots()
+
+rects1 = ax.bar(ind, acc_wo_stan, width, color='r')
+rects2 = ax.bar(ind + width, acc_w_stan, width, color='b')
+
+# Add some text for labels, title and axes ticks
+ax.set_ylabel('Accuracy', fontsize =16)
+ax.set_title('A Comparision of 5 Supervised Algorithms', fontsize =16)
+ax.set_xticks(ind + width / 2)
+ax.set_xticklabels(('KNN', 'Logistic Regression', 'XGB', 'Random Forest', 'SVM'), fontsize =16)
+plt.ylim((0,100))
+ax.legend((rects1[0], rects2[0]), ('Without Standardization', 'With Standardization'))
+
+autolabel(rects1)
+autolabel(rects2)
+
+plt.show()
---
+++ code/plot_rfc.py
@@ -0,0 +1,59 @@
+'''
+@author: Imran Raja Singh Bhuiyan
+@maintainer: Imran Raja Singh Bhuiyan
+@date: 19.07.2017
+'''
+
+# loading libraries
+import numpy as np
+from sklearn.model_selection import cross_val_score
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.ensemble import RandomForestClassifier
+
+# Input data
+raw_data = np.loadtxt('bddetails_15.csv', delimiter=',')
+
+# create design matrix X and target vector y
+data = raw_data[:, 0:-1]
+data = np.array(data)
+target = raw_data[:, 15].astype(np.int)
+target = np.array(target)
+
+# # standardization
+# for i in range(4, 15):
+#     data_j = list(data[:,i])
+#     for j in range(len(data_j)):
+#         min_j = min(data_j)
+#         max_j = max(data_j)
+#         jj = (data_j[j] - min_j)/(max_j - min_j)
+#         data[j, i] = jj
+
+def rfc_func(max_features, color):
+	test_scores_rfc = []
+	n_est = [i for i in range(1,100)]
+
+	for rs in n_est:
+		kfold = model_selection.KFold(n_splits=5, shuffle=True, random_state=38)
+		rfc = RandomForestClassifier(n_estimators=rs, max_features= max_features, random_state=38)
+		scores_rfc = cross_val_score(rfc, data, target, cv=kfold, scoring='accuracy')
+		scores_rfc = np.array(scores_rfc)
+		test_scores_rfc.append(scores_rfc.mean())
+
+	plt.plot(n_est, test_scores_rfc, '--bo', color = color, label='Max Features: {}'.format(max_features))
+	plt.legend(loc='upper right')
+
+
+rfc_func(3, 'b')	
+rfc_func(4, 'r')
+rfc_func(5, 'm')
+rfc_func(6, 'k')
+rfc_func(7, 'c')
+rfc_func(8, 'g')
+
+plt.title('Accuracy vs Number of Trees, for Random Forest Classifier', fontsize=16)
+plt.xlabel('Number of Trees', fontsize=16)
+plt.ylabel('Accuracy', fontsize=16)
+plt.legend(loc='lower right')
+plt.show()
+
